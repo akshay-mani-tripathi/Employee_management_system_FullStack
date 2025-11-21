811808c0c1ac3da7efb0cac05ced208e72d9aab2
Akshay mani tripathi <152083530+akshay-mani-tripathi@users.noreply.github.com>
2025-11-21 12:17:16 +0530

    Update API base URL to include '/api' suffix

--- ems/src/api/api.js
+++ ems/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_REACT_APP_API_BASE || "https://employee-management-system-fullstack-a18n.onrender.com",
+  baseURL: import.meta.env.VITE_REACT_APP_API_BASE || "https://employee-management-system-fullstack-a18n.onrender.com/api",
 });
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("ems_token");
